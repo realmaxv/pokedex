f0f8806e8fb1029a4dacf052c8221316abb8d81a
realmaxv <max.vossgaetter@gmx.de>
2025-04-14 14:56:52 +0200

    progressive app

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Meine ADHS App",
+        short_name: "ToDo",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#00bcd4",
+        icons: [
+          {
+            src: "/icons/pokeball.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pokeball.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
